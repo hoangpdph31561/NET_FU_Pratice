15bc6f1b318bec0d118afbd7bb317090c36780ac
Nguyen Le Hong Giang <giangnlh.forworking@gmail.com>
2026-01-30 09:30:50 +0700

    NET_FU_1 : Create new calculate method

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -208,3 +208,8 @@ Console.WriteLine("╚═══════════════════�
 Console.WriteLine("\n[Nhấn phím bất kỳ để kết thúc...]");
 Console.ReadKey();
 
+static int CalculateAge(int birthYear)
+{
+    int currentYear = DateTime.Now.Year;
+    return currentYear - birthYear;
+}
